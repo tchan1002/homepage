e29d93f26a834410e3163f4dec680f9ac351dced
tchan1002 <tchan0@uchicago.edu>
2025-01-16 22:12:58 +0000

    chat-integration

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -22,6 +22,38 @@ class EntriesController < ApplicationController
     the_entry.title = params.fetch("query_title")
     the_entry.body = params.fetch("query_body")
 
+    ask = "Given sample titles like 'On Time', 'On Idea-Generation', and 'On AI and Writing', give me a concise title for the following text. Include only the title in your response, no punctuation or padding: " + params.fetch("query_body")
+  
+    ask2 = params.fetch("query_body")
+
+    client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_KEY"))
+
+    messages = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You help rewrite journal entries to turn them from fragments into complete passages" },
+          { role: "user", content: ask }
+        ],
+      }
+    )
+
+    response = messages.fetch("choices").at(0).fetch("message").fetch("content").to_s
+    the_entry.title = response
+
+    messages2 = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You help rewrite journal entries to turn them from fragments into complete passages. You are however very keen on preserving the voice and idiosyncracies of any text you come across." },
+          { role: "user", content: ask2 }
+        ],
+      }
+    )
+
+    response2 = messages2.fetch("choices").at(0).fetch("message").fetch("content").to_s
+    the_entry.body = response2
+
     if the_entry.valid?
       the_entry.save
       redirect_to("/entries", { :notice => "Entry created successfully." })
